d78378d36af0a1054e19400e5d3f60710aa7110c
Benjamin Kha <ben.kha@outlook.com>
2018-03-13 12:46:54 -0700

    running just clusters and all data

--- code/rnn_python/main_RNN_training.py
+++ code/rnn_python/main_RNN_training.py
@@ -37,54 +37,54 @@ airports, airports_data = parse_data(normalization=normalization)
 real_test_data=get_real_test_target(tst_indexes)
 airports_clusters_data = {}
 airports_all_data = {'features_trn': [], 'targets_trn': [], 'features_tst': [], 'targets_tst': [],'targets_tst_real':[]}
-for air_id in airports:
-    cluster_id = airports_clusters[air_id]
-    if cluster_id in cluster_subset:
-        data = []
-        for rec in airports_data[air_id]:
-            data.append(rec)
-
-        features_trn, targets_trn, features_tst, targets_tst ,targets_tst_real = divide_to_sequence(data, tst_indexes, feature_num=n_in,
-                                                                                  PAST_DAYS=PAST_DAYS,real_test_data=real_test_data)
-
-
-        if airports_clusters_data.get(cluster_id, 0) == 0:
-            airports_clusters_data[cluster_id] = {}
-            airports_clusters_data[cluster_id]['features_trn'] = []
-            airports_clusters_data[cluster_id]['targets_trn'] = []
-            airports_clusters_data[cluster_id]['features_tst'] = []
-            airports_clusters_data[cluster_id]['targets_tst'] = []
-            airports_clusters_data[cluster_id]['targets_tst_real'] = []
-
-        for rec in features_trn:
-            airports_clusters_data[cluster_id]['features_trn'].append(rec)
-            airports_all_data['features_trn'].append(rec)
-        for rec in targets_trn:
-            airports_clusters_data[cluster_id]['targets_trn'].append(rec)
-            airports_all_data['targets_trn'].append(rec)
-        for rec in features_tst:
-            airports_clusters_data[cluster_id]['features_tst'].append(rec)
-            airports_all_data['features_tst'].append(rec)
-        for rec in targets_tst:
-            airports_clusters_data[cluster_id]['targets_tst'].append(rec)
-            airports_all_data['targets_tst'].append(rec)
-        for rec in targets_tst_real:
-            airports_clusters_data[cluster_id]['targets_tst_real'].append(rec)
-            airports_all_data['targets_tst_real'].append(rec)
-
-        #
-        # train RNN models for each airport data alone
-        #
-        if not os.path.isfile(out_path+"/airports/"+Experiment_KEY + '_' + str(air_id) + '_obj.save'):
-            model = MetaRNN(n_in=n_in, n_hidden=HIDDEN_LAYERS, n_out=n_out,
-                            learning_rate=0.005, learning_rate_decay=0.999,
-                            n_epochs=n_epochs, activation='tanh')
-
-            model.fit(features_trn, targets_trn, validation_frequency=1000, air_id=air_id)
-            directory = out_path + "/airports/" + Experiment_KEY + "/"
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-            model.save(directory, str(air_id) + '_obj.save')
+# for air_id in airports:
+#     cluster_id = airports_clusters[air_id]
+#     if cluster_id in cluster_subset:
+#         data = []
+#         for rec in airports_data[air_id]:
+#             data.append(rec)
+#
+#         features_trn, targets_trn, features_tst, targets_tst ,targets_tst_real = divide_to_sequence(data, tst_indexes, feature_num=n_in,
+#                                                                                   PAST_DAYS=PAST_DAYS,real_test_data=real_test_data)
+#
+#
+#         if airports_clusters_data.get(cluster_id, 0) == 0:
+#             airports_clusters_data[cluster_id] = {}
+#             airports_clusters_data[cluster_id]['features_trn'] = []
+#             airports_clusters_data[cluster_id]['targets_trn'] = []
+#             airports_clusters_data[cluster_id]['features_tst'] = []
+#             airports_clusters_data[cluster_id]['targets_tst'] = []
+#             airports_clusters_data[cluster_id]['targets_tst_real'] = []
+#
+#         for rec in features_trn:
+#             airports_clusters_data[cluster_id]['features_trn'].append(rec)
+#             airports_all_data['features_trn'].append(rec)
+#         for rec in targets_trn:
+#             airports_clusters_data[cluster_id]['targets_trn'].append(rec)
+#             airports_all_data['targets_trn'].append(rec)
+#         for rec in features_tst:
+#             airports_clusters_data[cluster_id]['features_tst'].append(rec)
+#             airports_all_data['features_tst'].append(rec)
+#         for rec in targets_tst:
+#             airports_clusters_data[cluster_id]['targets_tst'].append(rec)
+#             airports_all_data['targets_tst'].append(rec)
+#         for rec in targets_tst_real:
+#             airports_clusters_data[cluster_id]['targets_tst_real'].append(rec)
+#             airports_all_data['targets_tst_real'].append(rec)
+#
+#         #
+#         # train RNN models for each airport data alone
+#         #
+#         if not os.path.isfile(out_path+"/airports/"+Experiment_KEY + '_' + str(air_id) + '_obj.save'):
+#             model = MetaRNN(n_in=n_in, n_hidden=HIDDEN_LAYERS, n_out=n_out,
+#                             learning_rate=0.005, learning_rate_decay=0.999,
+#                             n_epochs=n_epochs, activation='tanh')
+#
+#             model.fit(features_trn, targets_trn, validation_frequency=1000, air_id=air_id)
+#             directory = out_path + "/airports/" + Experiment_KEY + "/"
+#             if not os.path.exists(directory):
+#                 os.makedirs(directory)
+#             model.save(directory, str(air_id) + '_obj.save')
 
 #
 # train RNN model for each cluster alone
@@ -99,7 +99,7 @@ for cluster_id in cluster_subset:
     features_tst = airports_clusters_data[cluster_id]['features_tst']
     targets_tst = airports_clusters_data[cluster_id]['targets_tst']
 
-    model.fit(features_trn, targets_trn, validation_frequency=1000)
+    model.fit(features_trn, targets_trn, validation_frequency=1000, air_id=str(cluster_id))
     directory = out_path + "/clusters/"+Experiment_KEY+"/"
     if not os.path.exists(directory):
         os.makedirs(directory)
@@ -118,6 +118,6 @@ model = MetaRNN(n_in=n_in, n_hidden=HIDDEN_LAYERS, n_out=n_out,
                 learning_rate=0.001, learning_rate_decay=0.999,
                 n_epochs=n_epochs, activation='tanh')
 
-model.fit(features_trn, targets_trn, validation_frequency=1000)
+model.fit(features_trn, targets_trn, validation_frequency=1000, air_id="all")
 
 model.save(out_path + "/all_data/", Experiment_KEY + '_obj.save')
